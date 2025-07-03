e72aa3cffdabeecf24a3da5bdb8dba10d486d091
Deeshan Liyanage <dtliyanage27@gmail.com>
2025-07-03 21:45:35 +0530

    Added the feature to accept incoming calls

--- move.py
+++ move.py
@@ -1,6 +1,9 @@
 import pyautogui
 import time
 
+fail = False
+incomingCall = False
+
 time.sleep(15) # Wait for the browser to load
 # Select the group
 try:
@@ -17,8 +20,43 @@ time.sleep(5)
 x, y = pyautogui.locateCenterOnScreen('CallButton.png',confidence=0.8)
 pyautogui.click(x,y)
 
+
+time.sleep(180) # wait till call fail
+try:
+    pyautogui.locateOnScreen('Call_Failure.png', confidence=0.8)
+    fail = True
+    print("Call failed, waiting for call...")
+
+    if fail:
+        closeButton = pyautogui.locateCenterOnScreen('close.png', confidence=0.8) 
+        pyautogui.click(closeButton)
+
+
+except pyautogui.ImageNotFoundException:
+    fail = False
+    print("Call successful")
+    
+
+if fail:
+    try:
+        pyautogui.locateOnScreen('Incoming_Call.png', confidence=0.8)
+        incomingCall = True
+    except pyautogui.ImageNotFoundException:
+        incomingCall = False
+    
+    while incomingCall == False:
+        time.sleep(10) # Wait for the next call 
+        try:
+            pyautogui.locateOnScreen('Incoming_Call.png', confidence=0.8)
+            x, y = pyautogui.locateCenterOnScreen('Accept_Call.png', confidence=0.8)
+            pyautogui.click(x, y) # Accept the call
+            print("Incoming call accepted")
+            incomingCall = True
+            fail = False
+        except pyautogui.ImageNotFoundException:
+            incomingCall = False
+
 # Check if Video and mic is on
-time.sleep(60) # Wait till the call is connected
 try:
     pyautogui.click('Mute.png')
     print("Unmuted successfully")
@@ -33,10 +71,8 @@ except pyautogui.ImageNotFoundException:
 
 # Go to full screen mode
 height, width = pyautogui.size() # Find the resolution of the screen
+time.sleep(15)
 pyautogui.moveTo(height/2, width/2, duration=1)
 pyautogui.click('Menu.png', duration=1)
 time.sleep(1)
 pyautogui.click('FullScreen.png', duration=1)
-
-
-
